3966f989e2a87bba9e8996fc334a9c8dd71b1164
vb <bonhomme.vincent@gmail.com>
2020-03-30 16:09:19 +0200

    +dependencies

---
+++ R/Momstats-package.R
@@ -0,0 +1,16 @@
+## usethis namespace: start
+#' @importFrom tibble tibble
+## usethis namespace: end
+NULL
+
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
